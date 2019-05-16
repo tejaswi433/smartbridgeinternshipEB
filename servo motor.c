62d4d4bd5ee1917d08c9c4c200f85e1c6a4ae619
tejaswi433 <50071163+tejaswi433@users.noreply.github.com>
2019-05-16 15:22:12 +0530

    Add files via upload

---
+++ servo motor.c
@@ -0,0 +1,24 @@
+
+
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+int main(void)
+{
+	TCCR1A |= (1<<COM1A1)|(1<<COM1B1)|(1<<WGM11);
+    TCCR1B |= (1<<WGM13)|(1<<WGM12)|(1<<CS11)|(1<<CS10);
+	ICR1=4999;
+    DDRD|=(1<<PD5);
+   
+    /* Replace with your application code */
+    while (1) 
+    {
+		OCR1A =97;//0
+		_delay_ms(1000);
+		OCR1A =280;//90
+		_delay_ms(1000);
+		OCR1A =535;//180
+		_delay_ms(1000);
+    }
+}
+
